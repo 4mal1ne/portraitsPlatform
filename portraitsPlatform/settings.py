1f798c4c9fbceac5c19db495ad316cc0c9324a78
4mal1ne <parieiko.vlad@gmail.com>
2021-10-09 00:36:53 +0300

    Wrote path for 'static' and static files dirs path. Added 'main-app' in 'instaled app'.

--- portraitsPlatform/settings.py
+++ portraitsPlatform/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app.apps.MainAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'portraitsPlatform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'templates', 'mainreactapp-ui/build']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    (BASE_DIR / 'mainreactapp-ui/build/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
