3f77ec93356adb64b90dfc5d31be1ec2dcb8f8ed
4mal1ne <parieiko.vlad@gmail.com>
2021-10-13 01:40:26 +0300

    Test code.Import react hooks and axios. Wrote GET request and render backend Json-DATA from Django.

--- mainreactapp-ui/src/App.js
+++ mainreactapp-ui/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/test-api/"
+        }).then(response => {
+            setPeople(response.data)
+        })
+    }, [])
+    return (
+        <div className="App">
+            <h1>test build react-django</h1>
+            <ul>
+                {people.map(p => (
+                    <li key={p.id}>{p.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
