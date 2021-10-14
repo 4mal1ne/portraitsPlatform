7f4279beeb114843c2f3a1eb088c99f94b86963f
4mal1ne <parieiko.vlad@gmail.com>
2021-10-15 02:52:14 +0300

    ViewSet from serializer for comments.

--- main_app/api/views.py
+++ main_app/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from .serializers import WorksSerializer
-from ..models import Works
+from .serializers import WorksSerializer, CommentsSerializer
+from ..models import Works, Comments
 
 
 class WorksViewSet(viewsets.ModelViewSet):
     queryset = Works.objects.all()
     serializer_class = WorksSerializer
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
