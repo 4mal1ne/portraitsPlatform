c20f152ca2cdecd076635f5ba763dc27bbd8a102
4mal1ne <parieiko.vlad@gmail.com>
2021-10-15 02:13:44 +0300

    Added filtering by works availability.

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class WorksManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def all(self):
+        return self.get_queryset().filter(ordering_option=True)
+
+
 class Works(models.Model):
     name = models.CharField(max_length=100)
     work_description = models.TextField(max_length=400, null=True, blank=True)
@@ -11,6 +20,7 @@ class Works(models.Model):
     ordering_option = models.BooleanField(default=True)
     publishing_time = models.DateTimeField(auto_now_add=True, editable=False)
     created_by = models.ForeignKey(User, editable=False, null=True, blank=True, on_delete=models.CASCADE)
+    objects = WorksManager()
 
     def __str__(self):
         return self.name
