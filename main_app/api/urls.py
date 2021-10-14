8a456206cbcafbfd22b7b02dc4f4520f22073d6b
4mal1ne <parieiko.vlad@gmail.com>
2021-10-15 02:50:58 +0300

    Add routing for comments

--- main_app/api/urls.py
+++ main_app/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from rest_framework import routers
-from .views import WorksViewSet
+from .views import WorksViewSet, CommentsViewSet
 
 router = routers.SimpleRouter()
 router.register('artworks', WorksViewSet, basename='artworks')
+router.register('comments', CommentsViewSet, basename='comments')
+
 urlpatterns = []
 urlpatterns += router.urls
 
