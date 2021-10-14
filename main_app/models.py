9e0c9d38db72115fe2e97e94446e0f7f8ef11814
4mal1ne <parieiko.vlad@gmail.com>
2021-10-15 01:30:47 +0300

    Created main models for workarts and comments. Run migrations.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Works(models.Model):
+    name = models.CharField(max_length=100)
+    work_description = models.TextField(max_length=400, null=True, blank=True)
+    photo = models.ImageField(upload_to='master_works/')
+    date = models.DateField()
+    price = models.IntegerField()
+    ordering_option = models.BooleanField(default=True)
+    publishing_time = models.DateTimeField(auto_now_add=True, editable=False)
+    created_by = models.ForeignKey(User, editable=False, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Comments(models.Model):
+    work = models.ForeignKey(Works, on_delete=models.CASCADE)
+    description = models.TextField(max_length=400, null=True, blank=True)
+    comment_author = models.ForeignKey(User, editable=False, null=True, blank=True, on_delete=models.CASCADE)
+    publishing_time = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.description
