cdb379b932d694af9b545df86567fe72912151c5
4mal1ne <parieiko.vlad@gmail.com>
2021-10-18 02:50:58 +0300

    Added new component

--- mainreactapp-ui/src/App.js
+++ mainreactapp-ui/src/App.js
@@ -1,26 +1,12 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./components/navigation/Navbar";
 
 function App() {
-    const [people, setPeople] = useState([])
-
-    useEffect(() => {
-        axios({
-            method: "GET",
-            url: "http://127.0.0.1:8000/api/test-api/"
-        }).then(response => {
-            setPeople(response.data)
-        })
-    }, [])
     return (
         <div className="App">
-            <h1>test build react-django</h1>
-            <ul>
-                {people.map(p => (
-                    <li key={p.id}>{p.name}</li>
-                ))}
-            </ul>
+            <Navbar />
         </div>
     );
 }
